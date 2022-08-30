c2570c19659b53b19682d2da7ed5bdf6902fbaa4
Moral <Moraledrhymes@gmail.com>
2022-08-30 12:00:52 +0200

    Add project files.

---
+++ Controllers/BenefitController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AssignmentOne.Models;
+
+namespace AssignmentOne.Controllers
+{
+    public class BenefitController : Controller
+    {
+        private AssignmentsEntities db = new AssignmentsEntities();
+
+        // GET: Benefit
+        public ActionResult Index()
+        {
+            var benefits = db.Benefits.Include(b => b.Category).Include(b => b.Duration);
+            return View(benefits.ToList());
+        }
+
+        // GET: Benefit/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Benefit benefit = db.Benefits.Find(id);
+            if (benefit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(benefit);
+        }
+
+        // GET: Benefit/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "MembershipName");
+            ViewBag.DurationId = new SelectList(db.Durations, "DurationId", "DurationName");
+            return View();
+        }
+
+        // POST: Benefit/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BenefitId,CategoryId,MemberPrefixCode,CategoryType,DurationId,Fees,CreditLimit,Max_Adult,MaxChild")] Benefit benefit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Benefits.Add(benefit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "MembershipName", benefit.CategoryId);
+            ViewBag.DurationId = new SelectList(db.Durations, "DurationId", "DurationName", benefit.DurationId);
+            return View(benefit);
+        }
+
+        // GET: Benefit/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Benefit benefit = db.Benefits.Find(id);
+            if (benefit == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "MembershipName", benefit.CategoryId);
+            ViewBag.DurationId = new SelectList(db.Durations, "DurationId", "DurationName", benefit.DurationId);
+            return View(benefit);
+        }
+
+        // POST: Benefit/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BenefitId,CategoryId,MemberPrefixCode,CategoryType,DurationId,Fees,CreditLimit,Max_Adult,MaxChild")] Benefit benefit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(benefit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "MembershipName", benefit.CategoryId);
+            ViewBag.DurationId = new SelectList(db.Durations, "DurationId", "DurationName", benefit.DurationId);
+            return View(benefit);
+        }
+
+        // GET: Benefit/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Benefit benefit = db.Benefits.Find(id);
+            if (benefit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(benefit);
+        }
+
+        // POST: Benefit/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Benefit benefit = db.Benefits.Find(id);
+            db.Benefits.Remove(benefit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/DurationController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AssignmentOne.Models;
+
+namespace AssignmentOne.Controllers
+{
+    public class DurationController : Controller
+    {
+        private AssignmentsEntities db = new AssignmentsEntities();
+
+        // GET: Duration
+        public ActionResult Index()
+        {
+            return View(db.Durations.ToList());
+        }
+
+        // GET: Duration/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Duration duration = db.Durations.Find(id);
+            if (duration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(duration);
+        }
+
+        // GET: Duration/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Duration/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DurationId,DurationName,DurationDays")] Duration duration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Durations.Add(duration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(duration);
+        }
+
+        // GET: Duration/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Duration duration = db.Durations.Find(id);
+            if (duration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(duration);
+        }
+
+        // POST: Duration/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DurationId,DurationName,DurationDays")] Duration duration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(duration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(duration);
+        }
+
+        // GET: Duration/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Duration duration = db.Durations.Find(id);
+            if (duration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(duration);
+        }
+
+        // POST: Duration/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Duration duration = db.Durations.Find(id);
+            db.Durations.Remove(duration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
